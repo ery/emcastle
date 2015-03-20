6ff0ee5ebab38ffa4399e7dd2076f7653dcea93a
Ery <erylei@gmail.com>
2015-03-20 15:27:27 +0800

    setup_headers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,15 @@
 class CustomersController < ApplicationController
+  after_action :setup_headers
+
   def index
     list = Customer.all.map(&:as_json)
     render json: {customers: list}
+  end
+
+  private
+
+  def setup_headers
+    host = request.headers['Origin'] || '*'
+    response.headers['Access-Control-Allow-Origin'] = host
   end
 end
